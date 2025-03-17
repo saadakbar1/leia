7c3191fa607ea486f783b6301ed66ebba1ae9f85
Gunda Abhishek <gunda.abhishek@phonepe.com>
2025-03-17 12:53:30 +0530

    adds default latest version in the definition & message processor constructor

--- leia-processor/src/main/java/com/grookage/leia/mux/DefaultMessageProcessor.java
+++ leia-processor/src/main/java/com/grookage/leia/mux/DefaultMessageProcessor.java
@@ -39,91 +39,91 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class DefaultMessageProcessor implements MessageProcessor {
 
-    private final String name;
-    private final long processingThresholdMs;
-    private final BackendNameResolver backendNameResolver;
-    private final MessageExecutorFactory executorFactory;
+	private final String name;
+	private final long processingThresholdMs;
+	private final BackendNameResolver backendNameResolver;
+	private final MessageExecutorFactory executorFactory;
 
-    protected DefaultMessageProcessor(String name,
-                                      long processingThresholdMs,
-                                      BackendNameResolver backendNameResolver,
-                                      MessageExecutorFactory executorFactory) {
-        Preconditions.checkNotNull(backendNameResolver, "Backend Resolver can't be null");
-        Preconditions.checkNotNull(executorFactory, "Executor Factory can't be null");
-        this.name = name;
-        this.processingThresholdMs = processingThresholdMs;
-        this.backendNameResolver = backendNameResolver;
-        this.executorFactory = executorFactory;
-    }
+	public DefaultMessageProcessor(String name,
+								   long processingThresholdMs,
+								   BackendNameResolver backendNameResolver,
+								   MessageExecutorFactory executorFactory) {
+		Preconditions.checkNotNull(backendNameResolver, "Backend Resolver can't be null");
+		Preconditions.checkNotNull(executorFactory, "Executor Factory can't be null");
+		this.name = name;
+		this.processingThresholdMs = processingThresholdMs;
+		this.backendNameResolver = backendNameResolver;
+		this.executorFactory = executorFactory;
+	}
 
-    protected boolean validBackends(List<String> backends) {
-        return null != backends && !backends.isEmpty();
-    }
+	protected boolean validBackends(List<String> backends) {
+		return null != backends && !backends.isEmpty();
+	}
 
-    protected boolean validExecutor(MessageExecutor executor) {
-        return null != executor;
-    }
+	protected boolean validExecutor(MessageExecutor executor) {
+		return null != executor;
+	}
 
-    private Map<MessageExecutor, List<LeiaMessage>> getExecutorMapping(List<LeiaMessage> messages) {
-        final var executorMapping = new HashMap<MessageExecutor, List<LeiaMessage>>();
-        messages.forEach(message -> {
-            final var backends = backendNameResolver.getEligibleBackends(message);
-            if (!validBackends(backends)) {
-                log.error("No backends found for message with schemaKey {} and tags {}", message.getSchemaKey(), message.getTags());
-                throw LeiaException.error(LeiaProcessorErrorCode.BACKENDS_NOT_FOUND);
-            }
-            backends.forEach(backend -> {
-                final var executor = executorFactory.getExecutor(backend).orElse(null);
-                if (!validExecutor(executor)) {
-                    log.error("No executor found for backend name {}", backend);
-                    throw LeiaException.error(LeiaProcessorErrorCode.EXECUTOR_NOT_FOUND);
-                }
-                executorMapping.computeIfAbsent(executor, k -> new ArrayList<>()).add(message);
-            });
-        });
-        return executorMapping;
-    }
+	private Map<MessageExecutor, List<LeiaMessage>> getExecutorMapping(List<LeiaMessage> messages) {
+		final var executorMapping = new HashMap<MessageExecutor, List<LeiaMessage>>();
+		messages.forEach(message -> {
+			final var backends = backendNameResolver.getEligibleBackends(message);
+			if (!validBackends(backends)) {
+				log.error("No backends found for message with schemaKey {} and tags {}", message.getSchemaKey(), message.getTags());
+				throw LeiaException.error(LeiaProcessorErrorCode.BACKENDS_NOT_FOUND);
+			}
+			backends.forEach(backend -> {
+				final var executor = executorFactory.getExecutor(backend).orElse(null);
+				if (!validExecutor(executor)) {
+					log.error("No executor found for backend name {}", backend);
+					throw LeiaException.error(LeiaProcessorErrorCode.EXECUTOR_NOT_FOUND);
+				}
+				executorMapping.computeIfAbsent(executor, k -> new ArrayList<>()).add(message);
+			});
+		});
+		return executorMapping;
+	}
 
-    public void processMessages(List<LeiaMessage> messages) {
-        final var executorMapping = getExecutorMapping(messages);
-        if (executorMapping.isEmpty()) {
-            log.debug("Haven't found any eligible executors with the set of messages {}", messages);
-            return;
-        }
-        final var futures = CompletableFuture.allOf(
-                executorMapping.entrySet().stream()
-                        .map(each -> CompletableFuture.runAsync(
-                                () -> each.getKey().send(each.getValue())))
-                        .toArray(CompletableFuture[]::new));
-        try {
-            futures.get(getProcessingThresholdMs(), TimeUnit.MILLISECONDS);
-        } catch (InterruptedException e) {
-            log.error("Couldn't perform the message processor execution. It exceeded the process duration specified : {}",
-                    getProcessingThresholdMs(), e);
-            Thread.currentThread().interrupt();
-            throw new IllegalStateException("Couldn't perform the message processor execution. It exceeded the process duration set at " + getProcessingThresholdMs());
-        } catch (Exception e) {
-            log.error("There is an exception while trying to process messages", e);
-            throw new IllegalStateException("There is an exception while trying to process messages", e);
-        }
-    }
+	public void processMessages(List<LeiaMessage> messages) {
+		final var executorMapping = getExecutorMapping(messages);
+		if (executorMapping.isEmpty()) {
+			log.debug("Haven't found any eligible executors with the set of messages {}", messages);
+			return;
+		}
+		final var futures = CompletableFuture.allOf(
+				executorMapping.entrySet().stream()
+						.map(each -> CompletableFuture.runAsync(
+								() -> each.getKey().send(each.getValue())))
+						.toArray(CompletableFuture[]::new));
+		try {
+			futures.get(getProcessingThresholdMs(), TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			log.error("Couldn't perform the message processor execution. It exceeded the process duration specified : {}",
+					getProcessingThresholdMs(), e);
+			Thread.currentThread().interrupt();
+			throw new IllegalStateException("Couldn't perform the message processor execution. It exceeded the process duration set at " + getProcessingThresholdMs());
+		} catch (Exception e) {
+			log.error("There is an exception while trying to process messages", e);
+			throw new IllegalStateException("There is an exception while trying to process messages", e);
+		}
+	}
 
-    @Override
-    public int hashCode() {
-        return this.getName().hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return this.getName().hashCode();
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-        final var thatKey = (DefaultMessageProcessor) obj;
-        return (thatKey.getName().equalsIgnoreCase(this.getName()));
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || obj.getClass() != this.getClass()) {
+			return false;
+		}
+		final var thatKey = (DefaultMessageProcessor) obj;
+		return (thatKey.getName().equalsIgnoreCase(this.getName()));
+	}
 }
 
 
